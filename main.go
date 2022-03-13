01d54e66e565eb14456f4750bf9e64ffc0f7408a
Jose M Varghese <josemvarghesee@gmail.com>
2022-03-14 00:23:49 +0400

    mongo insertion

--- main.go
+++ main.go
@@ -1,7 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Podcast struct {
+	ID     primitive.ObjectID `bson:"_id,omitempty"`
+	Title  string             `bson:"title,omitempty"`
+	Author string             `bson:"author,omitempty"`
+	Tags   []string           `bson:"tags,omitempty"`
+}
+type Episode struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty"`
+	Podcast     primitive.ObjectID `bson:"podcast,omitempty"`
+	Title       string             `bson:"title,omitempty"`
+	Description string             `bson:"description,omitempty"`
+	Duration    int32              `bson:"duration,omitempty"`
+}
 
 func main() {
 	fmt.Println("hello world")
+
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected")
+	defer client.Disconnect(ctx)
+
+	database := client.Database("go-mongo")
+	podcastsCollection := database.Collection("podcasts")
+	episodesCollection := database.Collection("episodes")
+
+	var episodes []Episode
+	cursor, err := episodesCollection.Find(ctx, bson.M{})
+	if err != nil {
+		panic(err)
+	}
+	if err = cursor.All(ctx, &episodes); err != nil {
+		panic(err)
+	}
+	fmt.Println(episodes)
+	podcast := Podcast{
+		Title:  "Test title",
+		Author: "Jose",
+		Tags:   []string{"TAG1", "TAG2"},
+	}
+	insertResult, err := podcastsCollection.InsertOne(ctx, podcast)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(insertResult)
 }
